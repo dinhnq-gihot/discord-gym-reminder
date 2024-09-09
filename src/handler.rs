120a43c9d1627d10288d3f8394d2209255b0d32f
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-09-09 17:50:41 +0700

    first commit

---
+++ src/handler.rs
@@ -0,0 +1,40 @@
+use serenity::all::MessageBuilder;
+use serenity::async_trait;
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            let channel = match msg.channel_id.to_channel(&ctx).await {
+                Ok(channel) => channel,
+                Err(why) => {
+                    println!("Error getting channel: {why:?}");
+
+                    return;
+                }
+            };
+
+            let response = MessageBuilder::new()
+                .push("User ")
+                .push_bold_safe(&msg.author.name)
+                .push(" used the 'ping' command in the ")
+                .mention(&channel)
+                .push(" channel")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod handler;
+use dotenv::dotenv;
+use handler::Handler;
+use serenity::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token = std::env::var("DISCORD_BOT_TOKEN").expect("Cannot get discord bot token!");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error createing client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
